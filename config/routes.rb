fa5c8453d72257b517ca5d97b5af21db6b679424
oooomilkywayoooo <105682588+oooomilkywayoooo@users.noreply.github.com>
2022-08-28 23:38:40 +0000

    [Add]ユーザーのhomeとaboutコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-devise_for :users,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
 
-devise_for :admin,skip: [:registrations, :passwords], controllers: {
-  sessions: "admin/sessions"
-}
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/' => 'public/homes#top'
+  get '/about' => 'public/homes#about'
 end
