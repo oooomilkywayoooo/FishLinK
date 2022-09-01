554d005071adaf3549e36f51ca6b2e900d812b60
oooomilkywayoooo <105682588+oooomilkywayoooo@users.noreply.github.com>
2022-09-01 23:37:37 +0000

    [Add]会員情報編集画面の修正

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -6,4 +6,16 @@ class Public::UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :profile_image)
+  end
 end
