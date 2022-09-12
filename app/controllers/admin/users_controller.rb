a6d8aa558c6a46255e75766cb5a63127cadead25
oooomilkywayoooo <105682588+oooomilkywayoooo@users.noreply.github.com>
2022-09-12 11:16:51 +0000

    [Add]管理者ユーザー一覧作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,14 @@
 class Admin::UsersController < ApplicationController
+before_action :authenticate_admin!
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+  end
 end
