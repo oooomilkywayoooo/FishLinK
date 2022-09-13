a3369986b30f652027e536b5bd73fd3a26e8046a
oooomilkywayoooo <105682588+oooomilkywayoooo@users.noreply.github.com>
2022-09-13 13:57:05 +0000

    [Add]管理者ユーザー詳細編集

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < ApplicationController
-before_action :authenticate_admin!
+  before_action :authenticate_admin!
 
   def index
     @users = User.all
@@ -10,5 +10,19 @@ before_action :authenticate_admin!
   end
 
   def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      flash[:notice] = "ステータスを変更しました"
+      redirect_to admin_user_path(user)
+    else
+      render :show
+    end
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:is_active)
   end
 end
