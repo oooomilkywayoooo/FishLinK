3d93ce64ea8aa40f44af5a765a03efa2eb0ec750
oooomilkywayoooo <105682588+oooomilkywayoooo@users.noreply.github.com>
2022-09-15 03:29:25 +0000

    [Add]管理者投稿、レビュー一覧と詳細作成

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,15 @@
 class Admin::PostsController < ApplicationController
   def index
+    @posts = Post.all.page(params[:page]).per(12)
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to admin_posts_path
   end
 end
