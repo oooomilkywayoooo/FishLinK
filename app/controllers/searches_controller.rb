f4e04303fc37af6a286cdcfb1ac5322f29de5ec0
oooomilkywayoooo <105682588+oooomilkywayoooo@users.noreply.github.com>
2022-09-26 13:32:51 +0000

    [Fix]管理者ページの不具合修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,8 @@
 class SearchesController < ApplicationController
-  before_action :authenticate_user!
-  
+
   def search
     @range = params[:range]
-    
+
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
     else
